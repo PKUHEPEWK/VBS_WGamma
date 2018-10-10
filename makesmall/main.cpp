ada841b1ff903b675af5be74fe88e8754a435b6a
freejiebao <jiexiao@pku.edu.cn>
2018-10-10 22:56:59 +0200

    change input diretory

--- makesmall/main.cpp
+++ makesmall/main.cpp
@@ -1,21 +1,21 @@
 #include "makesmall.h"
 int main(int argc, char* argv[]) {
     //gROOT->LoadMacro("xx.C");
-    TString dir = "root://cmseos.fnal.gov//eos/uscms/store/user/qliphy/jie/ori/";
-
+    //TString dir = "root://cmseos.fnal.gov//eos/uscms/store/user/qliphy/jie/ori/";
+    TString dir        = "/eos/uscms/store/user/qliphy/";
     TString infilename = argv[1];
     if (infilename.Contains(".root") == 0) {
         return 1;
     }
     TString outname = "out" + infilename;
 
-    cout << outname << endl;
+    std::cout << outname << std::endl;
 
     TFile*      file1 = TFile::Open(dir + infilename);
     TDirectory* dir1  = (TDirectory*)file1->Get("treeDumper");
     TTree*      tree1 = (TTree*)dir1->Get("PKUCandidates");
     makesmall   m1(tree1, outname);
-    cout << outname << endl;
+    std::cout << outname << std::endl;
     m1.Loop();
     m1.endJob();
     return 0;
